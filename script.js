2f86d74c3df525c1a90104fd5c0e776cc5e22607
Ekee47 <162979887+Ekee47@users.noreply.github.com>
2024-07-14 10:16:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkPassword() {
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+    
+    if (password === '1829') {
+        window.location.href = 'gallery.html'; // Redirect to the gallery page
+    } else {
+        errorMessage.style.display = 'block';
+    }
+}
