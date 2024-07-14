13cd4ea9b03e1692fcbd80d861b19fdd980a37bb
Ekee47 <162979887+Ekee47@users.noreply.github.com>
2024-07-14 16:59:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkPassword() {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
     
-    if (password === '1829') {
+    if (password === '1829<3') {
         window.location.href = 'gallery.html'; // Redirect to the gallery page
     } else {
         errorMessage.style.display = 'block';
